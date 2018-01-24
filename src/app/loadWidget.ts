77903b041d63822d206ab2d0152b3ac53f4f866f
Florian Rappl <rappl.florian@gmail.com>
2018-01-25 00:25:08 +0100

    Added the router

---
+++ src/app/loadWidget.ts
@@ -0,0 +1,25 @@
+import { MiniAppApi, MiniAppPackage } from './api';
+
+function requireModule(moduleName: string) {
+  switch (moduleName) {
+    case 'react':
+      return require('react');
+    case 'react-router-dom':
+      return require('react-router-dom');
+    case 'styled-components':
+      return require('styled-components');
+    default:
+      return undefined;
+  }
+}
+
+export function loadWidget(app: MiniAppPackage): MiniAppApi {
+  const mod = {
+    exports: undefined,
+  };
+  const importer = new Function('module', 'exports', 'require', app.content);
+  importer(mod, {}, requireModule);
+  return mod.exports || {
+    setup() {}
+  };
+}
