c0466f0f98d4e48aefff275796f2995c022500ee
Florian Rappl <rappl.florian@gmail.com>
2018-01-25 23:48:40 +0100

    Demonstrate event emitting and data sharing

--- widgets/calculator/src/calculatorTile.tsx
+++ widgets/calculator/src/calculatorTile.tsx
@@ -1,26 +1,37 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { PortalApi } from '../../../src/app';
 
 export interface CalculatorTileProps {
   rows: number;
   columns: number;
-  portal: any;
+  portal: PortalApi;
 }
 
 export interface CalculatorTileState {
   x: string;
   y: string;
   res: number;
+  temperature: number;
+}
+
+interface TemperatureChanged {
+  value: number;
 }
 
 export class CalculatorTile extends React.Component<CalculatorTileProps, CalculatorTileState> {
   constructor(props: CalculatorTileProps) {
     super(props);
+    const { portal } = props;
     this.state = {
       x: '0',
       y: '0',
       res: 0,
+      temperature: portal.data.temperature,
     };
+    portal.on<TemperatureChanged>('temperature-changed', ({ value }) => this.setState({
+      temperature: value,
+    }));
   }
 
   private changeX = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -32,18 +43,24 @@ export class CalculatorTile extends React.Component<CalculatorTileProps, Calcula
   };
 
   private calc(x: string, y: string) {
+    const { portal } = this.props;
+    const res = (+x || 0) + (+y || 0);
     this.setState({
       x,
       y,
-      res: (+x || 0) + (+y || 0),
+      res,
+    });
+
+    portal.emit('calculated', {
+      result: res,
     });
   }
 
   render() {
-    const { x, y, res } = this.state;
+    const { x, y, res, temperature } = this.state;
     return (
       <div>
-        <h3>Calculator</h3>
+        <h3>Calculator (at {temperature} °C)</h3>
         <div><input value={x} onChange={this.changeX} /></div>
         <div>+</div>
         <div><input value={y} onChange={this.changeY} /></div>
